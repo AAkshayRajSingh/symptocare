303bd61f37446250d7748ba0daed266fc048ba6f
Anguwala Akshay Raj Singh <67822687+AAkshayRajSingh@users.noreply.github.com>
2023-10-18 13:15:56 -0500

    ArtifyAI

--- src/pages/OTPVerification.js
+++ src/pages/OTPVerification.js
@@ -1,27 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Link, useNavigate } from "react-router-dom";
 import "/Users/a.akshayrajsingh/Desktop/ecommerce/store/src/pages/OPTVerification.css";
 
+const LOCKOUT_DURATION = 30000; // Lockout duration in milliseconds (30 seconds)
+
 export default function OTPVerification() {
   const [otp, setOtp] = useState("");
   const [error, setError] = useState(null);
-
+  const [locked, setLocked] = useState(false); // Add a locked state
   const navigate = useNavigate();
 
+  useEffect(() => {
+    let unlockTimeout;
+
+    // Check if the user is locked
+    if (locked) {
+      // Set a timeout to unlock the user after the specified duration
+      unlockTimeout = setTimeout(() => {
+        setLocked(false);
+        setError(null); // Clear any previous errors
+      }, LOCKOUT_DURATION);
+    }
+
+    return () => {
+      // Clear the unlock timeout when the component unmounts
+      clearTimeout(unlockTimeout);
+    };
+  }, [locked]);
+
   function validateForm() {
-    return otp.length === 4; // Assuming OTP is 4 digits
+    return otp.length === 4;
   }
 
   function handleSubmit(event) {
     event.preventDefault();
+
+    if (locked) {
+      setError(`You are locked out. Try again in ${LOCKOUT_DURATION / 1000} seconds.`);
+      return;
+    }
 
     if (otp === "0000") {
       // Successful OTP verification, navigate to the store
       navigate("/store");
     } else {
       setError("Incorrect OTP. Please try again.");
+      setLocked(true); // Lock the user after a failed attempt
     }
   }
 
@@ -34,7 +60,7 @@ export default function OTPVerification() {
           <Form.Label>Enter OTP</Form.Label>
           <Form.Control
             autoFocus
-            type="password" // Use password type to hide characters
+            type="password"
             value={otp}
             onChange={(e) => setOtp(e.target.value)}
           />
